b03eed9d12505bcb8738ed3e72e5833586035ec3
Alexander Viklund <viklu@kth.se>
2017-03-07 02:57:31 +0100

    Add AUTH_API_KEY setting

--- cashflow/settings.py
+++ cashflow/settings.py
@@ -96,6 +96,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_API_KEY = "fakekey-012345678910111213141516171819"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
