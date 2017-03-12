5b59a69fe3b7281f60ebbe7fa6f75259bb747f29
Alexander Viklund <viklu@kth.se>
2017-03-12 23:07:18 +0100

    Add authentication for getting users

--- expenses/misc_views.py
+++ expenses/misc_views.py
@@ -1,6 +1,9 @@
-from django.http import Http404, JsonResponse
+from django.http import Http404, JsonResponse, HttpResponseForbidden
 from expenses.models import Committee, Person
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from cashflow.dauth import has_permission
+
 
 def budget(request):
     if request.method != 'GET':
@@ -9,9 +12,15 @@ def budget(request):
 
 
 def user_by_username(request, username):
+    if not (request.user.username == username or has_permission("pay",request.user)):
+        return HttpResponseForbidden()
+
     if request.method != 'GET':
         raise Http404()
-    user = User.objects.get(username=username)
+    try:
+        user = User.objects.get(username=username)
+    except ObjectDoesNotExist:
+        raise Http404()
     return JsonResponse({'user': Person.objects.get(user=user).to_dict()})
 
 
