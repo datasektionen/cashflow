6dfff8a8673ac414eb35620e2fe43f4eb74a9f6d
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-13 18:11:29 +0100

    Basic read for expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from expenses.views.expense import ExpenseView
+from expenses.views.expense import ExpenseViewSet
 
 router = DefaultRouter()
-router.register('expense', ExpenseView, base_name='Expense')
+router.register('expense', ExpenseViewSet, base_name='Expense')
 
 urlpatterns = router.urls
--- expenses/views/expense.py
+++ expenses/views/expense.py
@@ -1,17 +1,33 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView, Response
+from rest_framework.views import Response
+from rest_framework.viewsets import GenericViewSet
 
 from expenses.models import Expense
 
 
-class ExpenseView(APIView):
+# noinspection PyUnusedLocal
+class ExpenseViewSet(GenericViewSet):
     authentication_classes = (SessionAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     """
     List all expenses the current user can access
     """
-    def get(self, request, format=None):
+    def list(self, request, **kwargs):
         return Response({'expenses': [exp.to_dict() for exp in Expense.objects.filter(owner__user=request.user)]})
+
+    """
+    Retrieve a single expense with parts and file information
+    """
+    def retrieve(self, request, pk, **kwargs):
+        try:
+            exp = Expense.objects.get(id=int(pk))
+        except ValueError as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(exp.to_dict())
 
