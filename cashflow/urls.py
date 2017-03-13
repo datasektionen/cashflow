1b27fea70eeffda3fbcc2856bb1b2b99fba8d785
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-13 22:00:24 +0100

    Merged frontend with django project

--- cashflow/urls.py
+++ cashflow/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from expenses import index
+
 urlpatterns = [
+    url(r'^', index.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include("expenses.urls"))
 ]
