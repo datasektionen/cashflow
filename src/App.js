d93dc90f4545c2a2ac44eb780963d6ca8478186b
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-15 02:36:42 +0100

    Some parts of layout in place

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
+import Dashboard from 'material-ui/svg-icons/action/dashboard';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { drawerOpen: true };
+        this.toggleDrawer = this.toggleDrawer.bind(this);
+    }
+
+    toggleDrawer() {
+        this.setState({
+            drawerOpen: !this.state.drawerOpen
+        });
+    }
+
     render() {
+        const contentStyle = { transition: 'margin-left 450ms cubic-bezier(0.23, 1, 0.32, 1)' };
+
+        if (this.state.drawerOpen)
+            contentStyle.marginLeft = 256;
+
         return (
             <div className="App">
-                <AppBar title={"Cashfl0w"} />
+                <Drawer open={this.state.drawerOpen}>
+                    <Subheader>Cashfl0w</Subheader>
+                    <MenuItem leftIcon={<Dashboard />} primaryText="Dashboard" />
+                    <dashboard />
+                    <Subheader>Inköp</Subheader>
+                    <MenuItem primaryText="Alla inköp" />
+                    <MenuItem primaryText="Bekräftade inköp" />
+                    <MenuItem primaryText="Lägg till inköp" />
+                </Drawer>
+
+                <div style={contentStyle} className="content">
+                    <AppBar title={"Cashfl0w"} onLeftIconButtonTouchTap={this.toggleDrawer} />
+                </div>
             </div>
         );
     }
