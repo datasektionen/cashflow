58254c8f82e7083de2429491fb8cf7f810f655c5
Alexander Viklund <viklu@kth.se>
2017-03-15 10:41:04 +0100

    Move login/logout urls

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,10 @@
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
 from expenses.views.comment import CommentViewSet
 from expenses.views.expense import ExpenseViewSet
+from expenses.views.misc import UserViewSet, budget, login, logout
 from expenses.views.pay import PaymentViewSet
-from expenses.views.user import UserViewSet
 
 router = DefaultRouter()
 router.register('expense', ExpenseViewSet, base_name='Expense')
@@ -11,3 +12,7 @@ router.register('payment', PaymentViewSet, base_name='Payment')
 router.register('comment', CommentViewSet, base_name='Comment')
 router.register('user', UserViewSet, base_name='User')
 urlpatterns = router.urls
+
+urlpatterns.append(url(r'^budget/$', budget))
+urlpatterns.append(url(r'^login/(.*)/$', login))
+urlpatterns.append(url(r'^logout/$', logout))
