c676511b77e73b6083dc97072fd691f8caeae215
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-15 20:14:40 +0100

    MUI CSS for the responsive grid

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
 
 class Dashboard extends Component {
-    constructor (props) {
-        super(props);
-    }
-
     render () {
         return (
-            <div>
-                <h2>Welcome to the Dashboard!</h2>
-            </div>
+            <Container fluid={true}>
+                <Row>
+                    <Col md="8">
+                        <Paper zDepth={2}>
+                            <h2>Welcome to the Dashboard!</h2>
+                        </Paper>
+                    </Col>
+                    <Col md="4">
+                        <Paper zDepth={2} style={{height: "80vh"}}>
+                            <h2>möp</h2>
+                        </Paper>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
