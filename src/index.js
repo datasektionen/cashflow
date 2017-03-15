3ce0cd81771f8d716807034e4129bd0a64c80669
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-15 02:01:16 +0100

    MUI base

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const Bootstrap = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(
-  <App />,
+  <Bootstrap />,
   document.getElementById('root')
 );
