df82d06bd600c4000b4ecad445aaf9953034f5b0
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-15 01:16:45 +0100

    System now forces user logins properly

--- expenses/index.py
+++ expenses/index.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        callback_url = request.scheme + '://' + request.get_host() + '/api/login/'
+        url = 'http://login2.datasektionen.se/login?callback=' + callback_url
+        return redirect(url)
+
     return render(request, 'index.html')
