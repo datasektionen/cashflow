af0661dfb59425333a41bf61a5a92078ccd1796d
Alexander Viklund <viklu@kth.se>
2017-03-16 13:48:05 +0100

    Add new views to urls

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
+from expenses.views.accounting import AccountingViewSet
+from expenses.views.attest import AttestViewSet
 from expenses.views.comment import CommentViewSet
 from expenses.views.expense import ExpenseViewSet
 from expenses.views.misc import budget, login, logout
-from expenses.views.user import UserViewSet
 from expenses.views.pay import PaymentViewSet
+from expenses.views.user import UserViewSet
 
 router = DefaultRouter()
 router.register('expense', ExpenseViewSet, base_name='Expense')
 router.register('payment', PaymentViewSet, base_name='Payment')
 router.register('comment', CommentViewSet, base_name='Comment')
 router.register('user', UserViewSet, base_name='User')
+router.register('attest', AttestViewSet, base_name='Attest')
+router.register('accounting',AccountingViewSet,AccountingViewSet)
 urlpatterns = router.urls
 
 urlpatterns.append(url(r'^budget/$', budget))
