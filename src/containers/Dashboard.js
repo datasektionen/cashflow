e229147117b8e71446dcb056ccf7721cfa54c90e
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-03-18 16:02:43 +0100

    Dashboard style fixes

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react';
-import Paper from 'material-ui/Paper';
 import { SET_APP_TITLE } from '../actions/actionTypes';
 import { connect } from "react-redux";
+
+import Paper from 'material-ui/Paper';
+import Subheader from 'material-ui/Subheader';
 
 class Dashboard extends Component {
     componentWillMount () {
@@ -10,28 +12,38 @@ class Dashboard extends Component {
 
     render () {
         return (
-            <Paper zDepth={1}>
-                <div style={{padding: "5px 20px"}}>
-                    <h1>Välkommen till Cashflow!</h1>
-                    <p>
-                        I cashflow kan du som är dFunktionär, eller på annat sätt engagerad inom sektionen
-                        och har tillgång till en budget, lägga upp kvitton på de utlägg som du vill ha
-                        återbetalning på från sektionen.
-                    </p>
-                    <p>
-                        För att du ska kunna få återbetalning på utlägg krävs två saker:
-                        <ul>
-                            <li>att du har kvar och laddar upp de kvitton som det rör sig om</li>
-                            <li>att du har fyllt i ett svenskt bankkontonummer under "Min profil"</li>
-                        </ul>
-                    </p>
-                    <p>
-                        Du kan ansöka om återbetalning för ett helt kvitto, eller delar av ett kvitto,
-                        från en eller flera budgetposter eller kostnadsställen tillhörande någon av
-                        sektionens nämnder.
-                    </p>
-                </div>
-            </Paper>
+            <div>
+                <Subheader>Välkommen till Cashflow!</Subheader>
+
+                <Paper zDepth={1}>
+                    <div style={{padding: "5px 20px"}}>
+                        <p>
+                            I cashflow kan du som är dFunktionär, eller på annat sätt engagerad inom sektionen
+                            och har tillgång till en budget, lägga upp kvitton på de utlägg som du vill ha
+                            återbetalning på från sektionen.
+                        </p>
+                    </div>
+                </Paper>
+
+                <Subheader>Hur du använder Cashflow</Subheader>
+
+                <Paper zDepth={1}>
+                    <div style={{padding: "5px 20px"}}>
+                        <p>
+                            För att du ska kunna få återbetalning på utlägg krävs två saker:
+                            <ul>
+                                <li>att du har kvar och laddar upp de kvitton som det rör sig om</li>
+                                <li>att du har fyllt i ett svenskt bankkontonummer under "Min profil"</li>
+                            </ul>
+                        </p>
+                        <p>
+                            Du kan ansöka om återbetalning för ett helt kvitto, eller delar av ett kvitto,
+                            från en eller flera budgetposter eller kostnadsställen tillhörande någon av
+                            sektionens nämnder.
+                        </p>
+                    </div>
+                </Paper>
+            </div>
         );
     }
 }
