71dc8663dbfbc389a4ce05a81a525e5b32d542e1
viklu <viklu@kth.se>
2017-07-10 23:27:03 +0200

    Remember to move template dir

--- cashflow/settings.py
+++ cashflow/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'cashflow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['build'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
