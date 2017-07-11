a3cd3212c0bedf254eadd736c950c2ee048e1f51
viklu <viklu@kth.se>
2017-07-11 12:58:24 +0200

    Add permissions to user views

--- expenses/human_views/user.py
+++ expenses/human_views/user.py
@@ -1,8 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse
 
+from cashflow.dauth import has_permission
 from expenses import models
 from expenses.forms import UserForm
 
@@ -10,9 +11,12 @@ from expenses.forms import UserForm
 def get_user(request, username):
     # TODO: Add permissions
     try:
+        user = models.User.objects.get_by_natural_key(username)
+        if not may_view_user(request, user):
+            return HttpResponseForbidden()
         return render(request, 'expenses/user.html',
                       {
-                          "showuser": models.User.objects.get_by_natural_key(username)
+                          "showuser": user
                       })
     except ObjectDoesNotExist:
         raise Http404("Användaren finns inte")
@@ -21,7 +25,8 @@ def get_user(request, username):
 def edit_user(request, username):
     try:
         user = models.User.objects.get_by_natural_key(username)
-
+        if not may_view_user(request, user):
+            return HttpResponseForbidden()
         if request.method == 'POST':
             received_form = UserForm(request.POST, instance=user.profile)
             if received_form.is_valid():
@@ -35,3 +40,7 @@ def edit_user(request, username):
             })
     except ObjectDoesNotExist:
         raise Http404("Användaren finns inte")
+
+
+def may_view_user(request, user_to_view):
+    return (request.user == user_to_view) or has_permission('admin', request)
