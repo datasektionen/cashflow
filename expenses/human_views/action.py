c682df88197ec8d5fee455a4502f7292cd6ea038
viklu <viklu@kth.se>
2017-07-18 18:45:43 +0200

    Rewrite query for attest-action

--- expenses/human_views/action.py
+++ expenses/human_views/action.py
@@ -5,16 +5,12 @@ from expenses import models
 
 def attest_overview(request):
     may_attest = request.user.profile.may_attest()
-    may_attest_expenses = []
-    for expense in models.Expense.objects.all():
-        if expense.owner.user == request.user:
-            continue
-        for expense_part in expense.expensepart_set.all():
-            if expense_part.budget_line.cost_centre.committee.name.lower() in may_attest and \
-                            expense_part.attested_by is None:
-                may_attest_expenses.append(expense)
-                break
+
+    may_attest_committees = [models.Committee.objects.get(name__iexact=committee) for committee in may_attest]
 
     return render(request, 'expenses/action_attest.html', {
-        'attestable_expenses': may_attest_expenses
+        'attestable_expenses': models.Expense.objects.filter(
+            expensepart__attested_by=None,
+            expensepart__budget_line__cost_centre__committee__in=may_attest_committees
+        ).distinct().exclude(owner__user=request.user)
     })
