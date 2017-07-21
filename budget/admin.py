88170cfa90400e1b0d33565ad1c679cfa6f000cf
viklu <viklu@kth.se>
2017-07-21 05:33:46 +0200

    Register new models in django-admin

--- budget/admin.py
+++ budget/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Year)
+admin.site.register(Committee)
+admin.site.register(CostCentre)
+admin.site.register(BudgetLine)
