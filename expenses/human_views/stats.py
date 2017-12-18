ec0b593039af13fe08b7fa4d2ae4b6bff4a4d5f4
Jonas Dahl <jonas@jdahl.se>
2017-12-18 20:58:59 +0100

    Get the best and not the worst

--- expenses/human_views/stats.py
+++ expenses/human_views/stats.py
@@ -11,5 +11,5 @@ def index(request):
         'year': models.Expense.objects.filter(reimbursement__isnull=False).aggregate(year=Sum('expensepart__amount'))['year'],
         'highscore': models.Profile.objects.filter(expense__reimbursement__isnull=False).annotate(
             total_amount=Sum('expense__expensepart__amount')
-        ).filter(total_amount__gte=0).order_by('total_amount')[:10]
+        ).filter(total_amount__gte=0).order_by('-total_amount')[:10]
     })
