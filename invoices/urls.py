1f3bbe9d2e566800d76679838db6079310f5e283
Jonas Dahl <jonas@jdahl.se>
2017-12-23 15:14:03 +0100

    New invoice dumb form

--- invoices/urls.py
+++ invoices/urls.py
@@ -4,5 +4,5 @@ from rest_framework.routers import DefaultRouter
 import invoices.views as views
 
 urlpatterns = [
-    url(r'^$', views.index, name='invoices-index'),
+    url(r'^new/$', views.new_invoice, name='invoices-new'),
 ]
--- invoices/views.py
+++ invoices/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {
+def new_invoice(request):
+    return render(request, 'invoices/new.html', {
     })
