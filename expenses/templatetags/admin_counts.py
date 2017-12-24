a34ca8b95244314c0aa5ce181743109fa9724a2c
Jonas Dahl <jonas@jdahl.se>
2017-12-25 00:10:56 +0100

    Admin counts includes invoices

--- expenses/templatetags/admin_counts.py
+++ expenses/templatetags/admin_counts.py
@@ -8,10 +8,10 @@ from expenses.models import *
 def counts(context):
     user = context['request'].user
     context['counts'] = {
-        'attest': Expense.attestable(user.profile.may_attest(), user).count(),
-        'confirm': Expense.confirmable().count(),
-        'pay': Expense.payable().count(),
-        'account': Expense.accountable(user.profile.may_account()).count()
+        'attest': Expense.attestable(user.profile.may_attest(), user).count() + Invoice.attestable(user.profile.may_attest(), user).count(),
+        'confirm': Expense.confirmable().count() + 0,
+        'pay': Expense.payable().count() + Invoice.payable().count(),
+        'account': Expense.accountable(user.profile.may_account()).count() + Invoice.accountable(user.profile.may_account()).count()
     }
 
     return ''
