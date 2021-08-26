1e5c299a3de36dbbd4492b18dcd9d98ca7cdab38
elmaxe <axel.elmarsson@gmail.com>
2021-08-26 19:14:41 +0200

    Fix invoice function

--- expenses/models.py
+++ expenses/models.py
@@ -143,16 +143,18 @@ class Profile(models.Model):
         return False
 
     def may_delete_invoice(self, invoice):
-        if 'attest-firmatecknare' in dauth.get_permissions(self.user) and invoice is not None:
-            return True
+	# TODO: Spara någon referens till att man tagit bort invoicen
+	# Emaila ägaren?
         if invoice.is_payed():
             return False
+        if 'attest-firmatecknare' in dauth.get_permissions(self.user) and invoice is not None:
+            return True
         if invoice.owner.user.username == self.user.username:
             return True
-        for invoice in invoice.invoicepart_set.all():
-            if self.may_attest(invoice_part):
-                return True
-        return False
+        for invoice_part in invoice.invoicepart_set.all():
+            if not self.may_attest(invoice_part):
+                return False
+        return True
 
     def may_be_viewed_by(self, user):
         return user.username == self.user.username or user.profile.is_admin()
