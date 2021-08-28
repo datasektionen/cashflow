b66a8ac131e8bc3865f0ef70817fa587303bfc7b
elmaxe <axel.elmarsson@gmail.com>
2021-08-28 17:18:59 +0200

    Allow both psql protocols

--- cashflow/settings.py
+++ cashflow/settings.py
@@ -108,7 +108,7 @@ if os.environ.get("DATABASE_URL"):  # Stuff for when running in Dokku.
 
     # Parse the DATABASE_URL env var.
     USER, PASSWORD, HOST, PORT, NAME = re.match(
-        "^postgresql://(?P<username>.*?):(?P<password>.*?)@(?P<host>.*?):(?P<port>\d+)/(?P<db>.*?)$",
+        "^postgres(ql)?://(?P<username>.*?):(?P<password>.*?)@(?P<host>.*?):(?P<port>\d+)/(?P<db>.*?)$",
         os.environ.get("DATABASE_URL", "")).groups()
 
     DATABASES = {
