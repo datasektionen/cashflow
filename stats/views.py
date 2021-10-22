80200f300ffe650319a2fb231277d4a6cc4a2ee7
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-10-22 18:10:07 +0200

    Filter expenses larger than 30000 kr on stats
    Vi ska inte uppmuntra folk till att göra stora utlägg. Det är
    en onödig risk och stora utlägg bör tas på faktura eller med
    företagskortet. Det är dessutom olagligt att använda sitt
    kreditkort för att få flygpoäng.
    
    (Notera att denna implementation filtrerar på summan av varje
    utbetalning, och inte varje individuellt utlägg, då det typ
    inte gick att implementera.)
    
    Co-authored-by: Herman Karlsson <herkarlsson@gmail.com>
    Co-authored-by: Douglas Fischer <anakin_skryter@hotmail.se>

--- stats/views.py
+++ stats/views.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 from django.db.models import Sum, Count
 from django.db.models.functions import TruncMonth, Coalesce
 from django.shortcuts import render
+from django.db.models import Q
 
 from expenses import models
 
@@ -25,11 +26,18 @@ def index(request):
     y1 = [x1.filter(month=date(datetime.now().year, i, 1)) for i in range(1, 13)]
     z1 = [0 if y1[i].count() == 0 else float(y1[i].get()['value']) for i in range(0, 12)]
 
+    highscore = models.Profile.objects \
+            .filter(expense__reimbursement__isnull=False) \
+            .exclude(expense__expensepart__amount__gte=30000) \
+            .annotate(total_amount=Sum('expense__expensepart__amount')) \
+            .filter(total_amount__gte=0) \
+            .order_by('-total_amount')[:10]
+
+    print(highscore)
+
     return render(request, 'stats/index.html', {
         'year': models.Expense.objects.filter(reimbursement__isnull=False).aggregate(year=Coalesce(Sum('expensepart__amount'), 0))['year'],
-        'highscore': models.Profile.objects.filter(expense__reimbursement__isnull=False).annotate(
-            total_amount=Sum('expense__expensepart__amount')
-        ).filter(total_amount__gte=0).order_by('-total_amount')[:10],
+        'highscore': highscore,
         'month_count': z,
         'month_sum': z1
     })
