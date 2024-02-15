45a278e29f8356d4d21d614e711e3cb9a6f299c1
Rafael Oliveira <rmfseo@kth.se>
2024-02-15 19:01:50 +0100

    feat: allow selecting year in monthly stats chart
    Closes #145

--- stats/urls.py
+++ stats/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from rest_framework.routers import DefaultRouter
 
 import stats.views as views
 
 urlpatterns = [
     url(r'^$', views.index, name='stats-index'),
     url(r'^summary/$', views.summary, name='stats-summary'),
+    url(r'^monthly/(?P<year>\d+)/$', views.monthly, name='stats-breakdown'),
 ]
