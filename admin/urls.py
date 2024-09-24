02a7c6dcd9a8ab05caec7c8d83b6972612149c1b
adasjo <adasjo@live.se>
2024-09-24 22:43:51 +0200

    add url redirects

--- admin/urls.py
+++ admin/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     url(r'^pay/$', views.pay_overview, name='admin-pay'),
     url(r'^account/$', views.account_overview, name='admin-account'),
 
+    url(r'^account/auth$', views.fortnox_auth, name='admin-account-auth'),
+    url(r'^account/auth/complete$', views.fortnox_auth_complete, name='admin-account-auth-complete'),
+
     url(r'^expense/(?P<pk>\d+)/verification/edit/$', views.edit_expense_verification, name='admin-expense-edit-verification'),
     url(r'^expense/(?P<expense_pk>\d+)/verification/$', views.set_verification, name='admin-expense-verification'),
     url(r'^expense/(?P<pk>\d+)/confirm/$', views.confirm_expense, name='admin-expense-confirm'),
