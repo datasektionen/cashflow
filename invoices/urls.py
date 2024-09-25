5f00220340c107841451a0fa5dd9c6566ace575a
lukmal <lukmal@kth.se>
2024-09-25 03:39:23 +0200

    Co-authored-by: Adam <adasjo@live.se>

--- invoices/urls.py
+++ invoices/urls.py
@@ -5,6 +5,7 @@ import invoices.views as views
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.get_invoice, name='invoices-show'),
+    url(r'^(?P<pk>\d+)/edit/$', views.edit_invoice, name='invoices-edit'),
     url(r'^new/$', views.new_invoice, name='invoices-new'),
     url(r'^new/confirmation/(?P<pk>\d+)/$', views.invoice_new_confirmation, name='invoices-new-confirmation'),
     url(r'^(?P<invoice_pk>\d+)/comment/$', views.new_comment, name='invoices-comment'),
