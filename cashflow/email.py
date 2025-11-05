d7b3e0e3a7bdd9f31a8451f39bec3a30eb564f17
Viktor Ekby <viktoe@datasektionen.se>
2025-11-05 15:52:33 +0100

    chore: update spam url

--- cashflow/email.py
+++ cashflow/email.py
@@ -5,7 +5,7 @@ def send_mail(recipient, subject, content):
     if not settings.SEND_EMAILS:
         return
 
-    requests.post(settings.SPAM_URL + '/api/sendmail', json={
+    requests.post(settings.SPAM_URL + '/api/legacy/sendmail', json={
         'from': 'cashflow-no-reply@datasektionen.se',
         'to': recipient,
         'subject': subject,
