9539772795d3092ef31a585679e2b465d7048ca7
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2026-01-09 15:00:59 +0100

    fix: uncomment nginx line in startup script

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 poetry run ./manage.py migrate
 poetry run gunicorn cashflow.wsgi --bind=0.0.0.0:8000 -t 600 --log-file - &
 
-# nginx -g 'daemon off;' &
-#
-wait
+nginx -g 'daemon off;' &
+
+wait -n
 exit $?
 
 # This scripts starts both python and nginx. If one exits, it kills the other
