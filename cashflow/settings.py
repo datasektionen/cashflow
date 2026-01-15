3dc8b4c476daafa56d91e140f5c7b2659d91abf2
Mathias Magnusson <mathias@magnusson.space>
2026-01-16 00:51:15 +0100

    this is pure trash

--- cashflow/settings.py
+++ cashflow/settings.py
@@ -44,6 +44,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = [ "https://cashflow.datasektionen.se" ]
+
 # Application definition
 
 INSTALLED_APPS = (
