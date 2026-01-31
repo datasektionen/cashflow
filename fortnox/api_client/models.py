bd7fdaf27b8eec859c29b91bdc303e6222dd99c7
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2026-01-31 23:10:06 +0100

    feat(fortnox): add Account (API) model

--- fortnox/api_client/models.py
+++ fortnox/api_client/models.py
@@ -1,4 +1,56 @@
-from pydantic import BaseModel
+from typing import Literal, Optional
+
+from pydantic import BaseModel, constr, conint, model_validator
+
+
+class Account(BaseModel):
+    # https://apps.fortnox.se/apidocs#tag/fortnox_Accounts
+    url: Optional[str]
+    Active: Optional[bool]
+    BalanceBroughtForward: Optional[float]
+    BalanceCarriedForward: Optional[float]
+    CostCenter: Optional[str]
+    CostCenterSettings: Optional[Literal['ALLOWED', 'MANDATORY', 'NOTALLOWED']]
+    Description: constr(min_length=1, max_length=200)
+    Number: conint(ge=1000, le=9999)
+    OpeningQuantities: Optional[list[OpeningQuantity]]
+    Project: Optional[str]
+    ProjectSettings: Optional[Literal['ALLOWED', 'MANDATORY', 'NOTALLOWED']]
+    QuantitySettings: Optional[Literal['ALLOWED', 'MANDATORY', 'NOTALLOWED']]
+    QuantityUnit: Optional[str]
+    SRU: Optional[int]
+    TransactionInformation: Optional[str]
+    TransactionInformationSettings: Optional[Literal['ALLOWED', 'MANDATORY', 'NOTALLOWED']]
+    VATCode: Optional[str]
+    Year: Optional[int]
+
+    @model_validator(mode='after')
+    def verify_settings(self):
+        match self.CostCenterSettings, self.CostCenter:
+            case 'ALLOWED', _:
+                pass
+            case 'MANDATORY', None:
+                raise ValueError('CostCenter cannot be empty when mandatory')
+            case 'NOTALLOWED', c if isinstance(c, str):
+                raise ValueError('CostCenter must be None when not allowed')
+
+        match self.ProjectSettings, self.Project:
+            case 'ALLOWED', _:
+                pass
+            case 'MANDATORY', None:
+                raise ValueError('Project cannot be empty when mandatory')
+            case 'NOTALLOWED', c if isinstance(c, str):
+                raise ValueError('Project must be None when not allowed')
+
+        match self.TransactionInformationSettings, self.TransactionInformation:
+            case 'ALLOWED', _:
+                pass
+            case 'MANDATORY', None:
+                raise ValueError('TransactionInformation cannot be empty when not allowed')
+            case 'NOTALLOWED', c if isinstance(c, str):
+                raise ValueError('TransactionInformation must be None when not allowed')
+
+        return self
 
 
 class Me(BaseModel):
@@ -8,6 +60,11 @@ class Me(BaseModel):
     Locale: str | None
     Name: str
     SysAdmin: bool
+
+
+class OpeningQuantity(BaseModel):
+    Balance: int
+    Project: str
 
 
 class AuthCodeGrant(BaseModel):
