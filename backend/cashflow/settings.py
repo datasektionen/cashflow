3ec3fc7689b247d307c7889a999647d85983ad9d
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2026-07-22 12:09:16 +0200

    fix: add secure_proxy_ssl_header setting (I like https)

--- backend/cashflow/settings.py
+++ backend/cashflow/settings.py
@@ -31,6 +31,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [*default_methods, "QUERY"]
