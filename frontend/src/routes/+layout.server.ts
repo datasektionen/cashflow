bdf67676fcca162946118c1cf34a01d5298a6b48
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2026-07-22 23:13:27 +0200

    fix: redirect to index page after login

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -5,6 +5,9 @@ export const load: LayoutServerLoad = ({ locals, url }) => {
 	if (!locals.user && url.pathname !== '/login') {
 		throw redirect(303, '/login');
 	}
+	if (locals.user && url.pathname === '/login') {
+		throw redirect(303, '/');
+	}
 	return {
 		user: locals.user
 	};
