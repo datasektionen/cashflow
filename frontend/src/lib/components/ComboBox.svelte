eaf899b4a5a250f1d3cec5f7c196ce16ffe1ca82
Hjalmar Fjällborg <hjalmar.fjallborg@gmail.com>
2026-08-02 15:02:56 +0200

    fix: combobox sizing on mobile (?)

--- frontend/src/lib/components/ComboBox.svelte
+++ frontend/src/lib/components/ComboBox.svelte
@@ -93,7 +93,7 @@ Wraps bits-ui's Combobox component. Supports fuzzy text search using fuse.js.
 		<Combobox.Portal>
 			<Combobox.Content
 				sideOffset={10}
-				class="focus-override border-muted h-96 w-full border-2 border-base-300 bg-base-200 dark:border-dark-base-300 dark:bg-dark-base-200"
+				class="focus-override border-muted max-h-[min(24rem,var(--bits-floating-available-height))] w-[var(--bits-floating-anchor-width)] border-2 border-base-300 bg-base-200 dark:border-dark-base-300 dark:bg-dark-base-200"
 			>
 				<Combobox.ScrollUpButton class="p-2 text-base dark:text-dark-base-text">
 					<ChevronUp class="m-auto" />
